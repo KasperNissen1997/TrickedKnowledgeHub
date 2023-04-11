4c2f79d22b3ed1e0703ba4b54b2511274ea29904
Kasper Nissen <KasperNissen1997@gmail.com>
2023-04-11 20:49:31 +0200

    Removed unnecessary whitespace, and reordered some properties.

--- TrickedKnowledgeHub/Model/Exercise.cs
+++ TrickedKnowledgeHub/Model/Exercise.cs
@@ -1,41 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TrickedKnowledgeHub.Model
 {
     public class Exercise
     {
         public string Title { get; set; }
-        public string Descrription { get; set; }
+        public string Description { get; set; }
         public string Material {get; set; }
-        public DateTime TimeStamp { get; set; }
+        public DateTime Timestamp { get; set; }
 
         public Employee Author { get; set; }
-        public FocusPoint FocusPoint { get; set; }
+        public Game? Game { get; set; }
+        public FocusPoint? FocusPoint { get; set; }
         public Rating Rating { get; set; }
-        public Game Game { get; set; }
 
-        public Exercise(Employee author, string title, string description, string material, Game game, FocusPoint focusPoint)
+        public Exercise(string title, string description, string material, Employee author, Game game, FocusPoint focusPoint)
         {
-            Author = author;
             Title = title;
-            Descrription = description;
+            Description = description;
             Material = material;
+
+            Author = author;
             Game = game;
             FocusPoint = focusPoint;
         }
-        
-        public Exercise(Employee author, string title, string description, string material, FocusPoint focusPoint)
-        {
-            Author = author;
-            Title = title;
-            Descrription = description;
-            Material = material;
-            FocusPoint = focusPoint;
-        }
-        
     }
 }
--- TrickedKnowledgeHub/Model/Rating.cs
+++ TrickedKnowledgeHub/Model/Rating.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrickedKnowledgeHub.Model
+﻿namespace TrickedKnowledgeHub.Model
 {
     public enum Rating
     {
@@ -13,6 +7,5 @@ namespace TrickedKnowledgeHub.Model
         Øvet = 3,
         Ekspert = 4,
         Talent = 5,
-
     }
 }
