21ffaddfe9726fab21a69ad4da3ca35f496aa243
adenstore <ndenstore@gmail.com>
2023-04-13 14:28:44 +0200

    Focuspoint klar gjort

--- TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
+++ TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace TrickedKnowledgeHub.Model.Repo
 {
-    internal class FocusPointRepository
+    internal class FocusPointRepository : Repository
     {
+        private List<FocusPoint> _focusPoints;
+
+        public FocusPointRepository() 
+        {
+            _focusPoints = new List<FocusPoint>();
+            Load();
+        }
+        public override void Load()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM FOCUSPOINT", con);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        FocusPoint focusPoint = new FocusPoint
+                            (
+                            dr["F_Title"].ToString()                       
+                            );
+
+                        _focusPoints.Add(focusPoint);
+                    };
+
+                }
+            }
+        }
+
+        public void Create(string title, LearningObjective parrent)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO FOCUSPOINT(F_Title,L_Title)" +
+                    "VALUES(@F_Title, @L_Title", con);
+                cmd.Parameters.Add("@F_Title", SqlDbType.NVarChar, 50).Value = title;
+                cmd.Parameters.Add("@L_Title", SqlDbType.NVarChar, 50).Value = parrent.Title;
+
+                cmd.ExecuteScalar();
+            }
+        }
+        public FocusPoint Retrieve(string title)
+        {
+            
+            foreach (FocusPoint focusPoint in _focusPoints)
+            {
+                if (title.Equals(focusPoint))
+                {
+                    return focusPoint;                    
+                }
+                
+            }
+            throw new ArgumentException($"no focuspoint found with that tittle{title}");
+        } 
+
+        public List<FocusPoint> RetrieveAll()
+        {
+            return new(_focusPoints);
+        }
+
+
     }
 }
