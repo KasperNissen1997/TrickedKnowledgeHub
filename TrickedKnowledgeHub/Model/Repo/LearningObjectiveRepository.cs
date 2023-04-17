1281f40c87158a6f2a821f99b41234b7ff60334c
Casper <capper2704@hotmail.dk>
2023-04-17 08:50:27 +0200

    Game and Learning repo is done, so far haha

--- TrickedKnowledgeHub/Model/Repo/LearningObjectiveRepository.cs
+++ TrickedKnowledgeHub/Model/Repo/LearningObjectiveRepository.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace TrickedKnowledgeHub.Model.Repo
 {
-    internal class LearningObjectiveRepository
+    public class LearningObjectiveRepository : Repository
     {
+        private List<LearningObjective> learningObjectives = new List<LearningObjective>();
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(string title)
+        {
+            learningObjectives.Add(new LearningObjective(title));
+        }
+
+        public LearningObjective Retrive(string title)
+        {
+            foreach (LearningObjective learningObjective in learningObjectives)
+            {
+                if (learningObjective.Title == title)
+                {
+                    return learningObjective;
+                }
+            }
+
+            throw new ArgumentException($"No learningObjective with title {title} found.");
+        }
+
+        public LearningObjective RetriveAll()
+        {
+            foreach (LearningObjective learningObjective in learningObjectives)
+            {
+                return new LearningObjective(learningObjective.Title);
+            }
+
+            throw new ArgumentException($"No more learningObjectives could be found  ");
+        }
+
     }
 }
