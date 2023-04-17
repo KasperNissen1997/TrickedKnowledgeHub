59589eae215699a33c0bed1f072e469d12b196b1
adenstore <ndenstore@gmail.com>
2023-04-17 10:02:20 +0200

    fjernet og ændre dele af koden

--- TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
+++ TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace TrickedKnowledgeHub.Model.Repo
 {
@@ -12,17 +13,17 @@ namespace TrickedKnowledgeHub.Model.Repo
         public FocusPointRepository() 
         {
             _focusPoints = new List<FocusPoint>();
-            Load();
+            Load(); // when the repository is called the load methode runs and makes the list with the focus points
         }
         public override void Load()
         {
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM FOCUSPOINT", con);
+                SqlCommand cmd = new SqlCommand("SELECT F_Title FROM FOCUSPOINT", con);
                 using (SqlDataReader dr = cmd.ExecuteReader())
                 {
-                    while (dr.Read())
+                    while (dr.Read()) // As long there something to read from the server it runs and save it to the list
                     {
                         FocusPoint focusPoint = new FocusPoint
                             (
@@ -42,9 +43,9 @@ namespace TrickedKnowledgeHub.Model.Repo
             {
                 con.Open();
                 SqlCommand cmd = new SqlCommand("INSERT INTO FOCUSPOINT(F_Title,L_Title)" +
-                    "VALUES(@F_Title, @L_Title", con);
-                cmd.Parameters.Add("@F_Title", SqlDbType.NVarChar, 50).Value = title;
-                cmd.Parameters.Add("@L_Title", SqlDbType.NVarChar, 50).Value = parrent.Title;
+                    "VALUES(@F_Title, @L_Title", con); // tells the server what values you insert into.
+                cmd.Parameters.AddWithValue("@F_Title", title); //adds the vaule to server.
+                cmd.Parameters.AddWithValue("@L_Title", parrent.Title);
 
                 cmd.ExecuteScalar();
             }
@@ -52,18 +53,18 @@ namespace TrickedKnowledgeHub.Model.Repo
         public FocusPoint Retrieve(string title)
         {
             
-            foreach (FocusPoint focusPoint in _focusPoints)
+            foreach (FocusPoint focusPoint in _focusPoints) //looks into the list to see if a focus point with that name
             {
-                if (title.Equals(focusPoint))
+                if (title.Equals(focusPoint.Title))
                 {
                     return focusPoint;                    
                 }
                 
             }
-            throw new ArgumentException($"no focuspoint found with that tittle{title}");
+            throw new ArgumentException($"no focuspoint found with that tittle:{title}");
         } 
 
-        public List<FocusPoint> RetrieveAll()
+        public List<FocusPoint> RetrieveAll() // not use if there need to be a Exception
         {
             return new(_focusPoints);
         }
