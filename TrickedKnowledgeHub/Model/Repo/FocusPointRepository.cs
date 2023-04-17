721943ff0719dd5c0c78f5df5e3be13008aa6193
Kasper Nissen <KasperNissen1997@gmail.com>
2023-04-17 09:59:30 +0200

    Fixed some errors found while inspecting the previous PR.

--- TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
+++ TrickedKnowledgeHub/Model/Repo/FocusPointRepository.cs
@@ -5,7 +5,7 @@ using Microsoft.Data.SqlClient;
 
 namespace TrickedKnowledgeHub.Model.Repo
 {
-    internal class FocusPointRepository : Repository
+    public class FocusPointRepository : Repository
     {
         private List<FocusPoint> _focusPoints;
 
@@ -14,60 +14,54 @@ namespace TrickedKnowledgeHub.Model.Repo
             _focusPoints = new List<FocusPoint>();
             Load();
         }
+
         public override void Load()
         {
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM FOCUSPOINT", con);
+
+                SqlCommand cmd = new SqlCommand("SELECT (F_Title) FROM FOCUSPOINT", con);
+
                 using (SqlDataReader dr = cmd.ExecuteReader())
                 {
                     while (dr.Read())
                     {
-                        FocusPoint focusPoint = new FocusPoint
-                            (
-                            dr["F_Title"].ToString()                       
-                            );
+                        FocusPoint focusPoint = new FocusPoint(dr["F_Title"].ToString());
 
                         _focusPoints.Add(focusPoint);
-                    };
-
+                    }
                 }
             }
         }
 
-        public void Create(string title, LearningObjective parrent)
+        public void Create(string title, LearningObjective parent)
         {
             using (SqlConnection con = new SqlConnection(ConnectionString))
             {
                 con.Open();
+
                 SqlCommand cmd = new SqlCommand("INSERT INTO FOCUSPOINT(F_Title,L_Title)" +
                     "VALUES(@F_Title, @L_Title", con);
                 cmd.Parameters.Add("@F_Title", SqlDbType.NVarChar, 50).Value = title;
-                cmd.Parameters.Add("@L_Title", SqlDbType.NVarChar, 50).Value = parrent.Title;
+                cmd.Parameters.Add("@L_Title", SqlDbType.NVarChar, 50).Value = parent.Title;
 
-                cmd.ExecuteScalar();
+                cmd.ExecuteNonQuery();
             }
         }
+
         public FocusPoint Retrieve(string title)
         {
-            
             foreach (FocusPoint focusPoint in _focusPoints)
-            {
-                if (title.Equals(focusPoint))
-                {
-                    return focusPoint;                    
-                }
-                
-            }
+                if (title.Equals(focusPoint.Title))
+                    return focusPoint;
+
             throw new ArgumentException($"no focuspoint found with that tittle{title}");
-        } 
+        }
 
         public List<FocusPoint> RetrieveAll()
         {
             return new(_focusPoints);
         }
-
-
     }
 }
