1b4139929a70668e0a1d813c5886bf9ce9b4e796
Nikolai 'Møller <nikolaimoeller@hotmail.dk>
2023-04-17 10:03:09 +0200

    All methods done, comments added

--- TrickedKnowledgeHub/Model/Repo/EmployeeRepository.cs
+++ TrickedKnowledgeHub/Model/Repo/EmployeeRepository.cs
@@ -18,7 +18,7 @@ namespace TrickedKnowledgeHub.Model.Repo
 
         public EmployeeRepository()
         {
-
+            Load();
         }
 
         public override void Load()
@@ -28,17 +28,17 @@ namespace TrickedKnowledgeHub.Model.Repo
                 employeeList.Clear(); //Clears list before loading, no duplicates.
 
                 con.Open(); //Open connection
-                SqlCommand cmd = new SqlCommand("SELECT Name, Mail, Nickname, Password, Type  FROM EMPLOYEE", con);
+                SqlCommand cmd = new SqlCommand("SELECT Name, Mail, Nickname, Password, Type  FROM EMPLOYEE", con); //SQL Query run at execution
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
-                    while (reader.Read())
+                    while (reader.Read()) //While reader reads
                     {
                         Employee employee = new Employee(
-                            reader["Name"].ToString(),
+                            reader["Name"].ToString(), //SQL string is different from C# string, needs to be converted
                             reader["Mail"].ToString(),
                             reader["Nickname"].ToString(),
-                            reader["Password"].ToString(),
-                            (EmployeeType)Enum.Parse(typeof(EmployeeType), reader["Type"].ToString())); //Trying to convert string to Enum "Type"
+                            reader["Password"].ToString(),       
+                            (EmployeeType)Enum.Parse(typeof(EmployeeType), reader["Type"].ToString())); //convert string to Enum "EmployeeType"
                         employeeList.Add(employee);
                     }
                 }
@@ -47,20 +47,48 @@ namespace TrickedKnowledgeHub.Model.Repo
 
         public void Create(string email, string name, string nickname, string password, EmployeeType type)
         {
-
-        }
-        public Retrieve(email : string)
-        {
-
-        }
-        public RetrieveAll()
-        {
-            private 
-            foreach(Employee emp in employeeList)
+            using (SqlConnection con = new SqlConnection(connectionString))
             {
-                
+
+                con.Open(); //Open connection
+                SqlCommand cmd = new SqlCommand("INSERT INTO EMPLOYEE (Name, Mail, Nickname, Password, Type) " + //SQL query ran at execution
+                                                "VALUES(@Mail, @Name, @Nickname, @Password, @Type)", con); //values references @ in code block below
+                {
+
+                    cmd.Parameters.AddWithValue("@Mail", email); //cmd.Parameters{Get;}.AddWithValue(ParameterName, object value)
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Nickname", nickname);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    cmd.Parameters.AddWithValue("@Type", type.ToString());
+                    
+                }
             }
-            return 
         }
-}
+        public Employee Retrieve(string email) //Retrieves Employee based on string parameter
+        {
+            foreach (Employee employee in employeeList) //foreach Employee in list
+            {
+                if (email == employee.Mail) //if given string equals selected employee mail
+                {
+                    return employee; //return selected employee
+                }
+
+            }
+            throw new ArgumentException($"Could not find employee with mail: {email} ");
+
+        }
+        public List<Employee> RetrieveAll() //Retrieves all Employees, aka returning the whole list
+        {
+
+            if (employeeList != null) //if list is not empty
+            {
+                return new(employeeList); //return list
+            }
+            else
+            {
+                throw new ArgumentException($"List is empty or not loaded properly"); //exception message
+
+            }
+        }
+    }
 }
