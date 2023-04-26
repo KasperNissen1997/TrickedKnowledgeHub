0c06f84d834fb376add4ea9c5f0592d323717020
Oguz <oguz2863@gmail.com>
2023-04-26 08:10:51 +0200

    Fixed json connection?

--- TrickedKnowledgeHub/Model/Repo/Repository.cs
+++ TrickedKnowledgeHub/Model/Repo/Repository.cs
@@ -5,14 +5,13 @@ namespace TrickedKnowledgeHub.Model.Repo
 {
     public abstract class Repository : IRepository
     {
-        // private IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        private IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
         protected SqlConnection GetConnection()
         {
-            // string cs = config["Data:DefaultConnection:ConnectionString"];
-            string cs = "Server=10.56.8.36; Database=P3_DB_2023_06; User Id=P3_PROJECT_USER_06; Password=OPENDB_06; TrustServerCertificate=true";
-            
-            SqlConnection connection = new SqlConnection(cs);
+            string? ConnectionString = config.GetConnectionString("MyDBConnection");
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
             
             return connection;
         }
