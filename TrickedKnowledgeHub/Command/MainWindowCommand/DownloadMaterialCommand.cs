856ed0c05ce7c8c0986c2e6b460cfa77f62d4865
Skadefryd21 <nikolaimoeller@hotmail.dk>
2023-05-02 22:39:44 +0200

    DownloadMaterialCommand methods has been implemented and ready to be used.
    Work that needs to be done:
    ViewModel needs to reference and instantiate the command for the given exercise.

--- TrickedKnowledgeHub/Command/MainWindowCommand/DownloadMaterialCommand.cs
+++ TrickedKnowledgeHub/Command/MainWindowCommand/DownloadMaterialCommand.cs
@@ -3,11 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Input;
+using TrickedKnowledgeHub.Model;
+using TrickedKnowledgeHub.Model.Repo;
 
 namespace TrickedKnowledgeHub.Command.MainWindowCommand
 {
-    public class DownloadMaterialCommand
+    public class DownloadMaterialCommand : ICommand
     {
+        public event EventHandler CanExecuteChanged;
 
+        private Exercise exercise;
+
+        public ExerciseRepository exerciseRepository = RepositoryManager.ExerciseRepository; //Needed for calling methods in repo
+
+        byte[] selectedByte; //Field for material
+
+        string fileName; //File name
+
+        public DownloadMaterialCommand(Exercise exercise)
+        {
+            this.exercise = exercise;
+        }
+
+        public bool CanExecute(object parameter) //Makes sure the command cannot execute in case there is no exercise being passed
+        {
+            if (this.exercise == null) { return false; }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public void Execute(object parameter)
+        {
+            // Cast the parameter to the Exercise type
+            Exercise exercise = parameter as Exercise;
+
+            // Access the exercise object
+            if (exercise != null)
+            {
+                // Download the material for the specific exercise
+                exercise = exerciseRepository.Retrieve(exercise.ExerciseID);
+
+                if (exercise.Material != null)
+                {
+                    selectedByte = exercise.Material;
+
+                    fileName = exercise.Title;
+
+                    // Get the path to the user's Downloads folder
+                    string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+
+                    string filePath = Path.Combine(downloadsPath, fileName + ".docx");
+
+                    File.WriteAllBytes(filePath, selectedByte);
+                }
+            }
+        }
     }
 }
