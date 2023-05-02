70051adfbc5e8f689696e83a00dbc01f16e63fa1
Stokn9x <capper2704@hotmail.dk>
2023-05-02 09:54:49 +0200

    Live feed (#93)
    * Added a feed of the existing exercises.
    
    ---------
    
    Co-authored-by: adenstore <ndenstore@gmail.com>
    Co-authored-by: Kasper Nissen <KasperNissen1997@gmail.com>

--- TrickedKnowledgeHub/ViewModel/MainWindowViewVM.cs
+++ TrickedKnowledgeHub/ViewModel/MainWindowViewVM.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TrickedKnowledgeHub.Command.MainWindowCommand;
 using TrickedKnowledgeHub.Model.Repo;
 using TrickedKnowledgeHub.ViewModel.Domain;
+using TrickedKnowledgeHub.Model;
 
 namespace TrickedKnowledgeHub.ViewModel
 {
@@ -16,12 +17,23 @@ namespace TrickedKnowledgeHub.ViewModel
         public Create_exercise_window create_exercise_window;
         public OpenCreateExerciseViewCmd OpenCreateExerciseViewCmd { get; set; } = new OpenCreateExerciseViewCmd();
 
+        private List<ExerciseVM> _exerciseVM;
+        public List<ExerciseVM> ExerciseVMs
+        {
+            get { return _exerciseVM; }
+            set { _exerciseVM = value; }
+        }
+
         public MainWindowViewVM()
         {
             //You need to enter the email of a active user in the database, so be sure the email is in the database.
             ActiveUser = new(RepositoryManager.EmployeeRepository.Retrieve("nikolai@gmail.com"));
 
             create_exercise_window = new Create_exercise_window();
+            ExerciseVMs = new();
+            foreach (Exercise exercise in RepositoryManager.ExerciseRepository.RetrieveAll())
+                ExerciseVMs.Add(new ExerciseVM(exercise));
+
         }
     }
 }
