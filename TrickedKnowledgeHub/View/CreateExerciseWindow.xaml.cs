9b149eb680315613021ce84fc4adddec8a8e5b1a
Stokn9x <capper2704@hotmail.dk>
2023-05-04 09:32:10 +0200

    Fix #ez (#112)

--- TrickedKnowledgeHub/View/CreateExerciseWindow.xaml.cs
+++ TrickedKnowledgeHub/View/CreateExerciseWindow.xaml.cs
@@ -47,6 +47,7 @@ namespace TrickedKnowledgeHub
             string message = "Øvelsen er hermed gemt :)";
             string title = "Gemt Øvelse";
             MessageBox.Show(message, title);
+            Close();
         }
     }
 }
