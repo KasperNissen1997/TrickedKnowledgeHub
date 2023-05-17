c65e659eff675c48d4ab2e4528362cfdb6a8308d
Casper <capper2704@hotmail.dk>
2023-05-17 10:09:40 +0200

    Login view is  done bichesssssss! aHAHA Kasper Nisser

---
+++ TrickedKnowledgeHub/Command/LoginBNTCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TrickedKnowledgeHub.ViewModel;
+
+namespace TrickedKnowledgeHub.Commands
+{
+    public class LoginBNTCommand : ICommand
+    {
+            
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove {CommandManager.RequerySuggested -= value; }
+        }
+
+
+
+        public bool CanExecute(object? parameter)
+        {
+            if(parameter is LoginViewModel vm)
+            {
+                bool lightCheck = false;
+
+                if (!string.IsNullOrEmpty(vm.Password) && !string.IsNullOrEmpty(vm.Username))
+                   lightCheck = true;
+                CommandManager.InvalidateRequerySuggested();
+
+                return lightCheck;
+            }
+            else
+            {
+                return false;
+                
+            } 
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is LoginViewModel vm)
+            {
+                vm.Username = "hejsa";
+            }
+            else
+            {
+                throw new ArgumentException("Wrong input");
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
